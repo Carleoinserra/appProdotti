8a33b78e7413dbfc7241e4ac1f4501ed4c5972f2
Carleoinserra <inserracarlo@gmail.com>
2024-09-19 11:54:25 +0200

    Add files via upload

--- appProdotti/prodotti.py
+++ appProdotti/prodotti.py
@@ -9,6 +9,22 @@ class prodotto:
         self.pezzi = pezzi
         self.prodottiV = prodottiV
 
+class prodottiV:
+    def __init__(self, nome, marca, prezzo, url):
+        self.nome = nome
+        self.marca = marca
+        self.prezzo = prezzo
+        self.url = url
+
+
+    def setPezzi(self, pezziV):
+        self.pezziV = pezziV
+
+
+    def getPezzi(self):
+        return self.pezziV
+
+
 
 
 
@@ -121,6 +137,83 @@ def updatePezzi():
 
 
         return(index())
+
+
+@app.route("/buy", methods=['POST', 'GET'])
+def buy():
+    if request.method == 'POST':
+        listaP = request.form.getlist('prodA')
+        print(listaP)
+        listaId = request.form.getlist('prodN')
+        print(listaId)
+    listaPr = []
+    listaPrV = []
+    for i, num in enumerate(listaP):
+        if num != "0":
+
+            mycursor = mydb.cursor()
+            sql = "UPDATE prodotti SET pezzi = pezzi - %s  WHERE id = %s"
+            val = (num, listaId[i])
+            mycursor.execute(sql, val)
+            mydb.commit()
+
+            print(mycursor.rowcount, "record update.")
+
+            mycursor = mydb.cursor()
+            sql = "UPDATE prodotti SET prodottiV = prodottiV + %s  WHERE id = %s"
+            val = (num, listaId[i])
+            mycursor.execute(sql, val)
+            mydb.commit()
+
+            print(mycursor.rowcount, "record update.")
+            listaPr.append(listaId[i])
+            listaPrV.append(num)
+            '''
+            mycursor = mydb.cursor()
+
+            mycursor.execute("SELECT * FROM prodotti")
+
+            myresult = mycursor.fetchall()
+            idS = (listaId[i])
+            for j in myresult:
+
+                if (j[0]) == idS:
+                    p1 = prodottiV(j[1], j[2], j[3], j[4], num)
+                    print("ciao")
+                    listaPr.append(p1)
+'''
+    listaVendita = []
+
+    for p1 in listaPr:
+        mycursor = mydb.cursor()
+
+        sql = ("SELECT * FROM prodotti WHERE id = %s")
+
+        val = (p1,)
+        mycursor.execute(sql, val)
+        myresult = mycursor.fetchall()
+        for pr2 in myresult:
+            p4 = prodottiV(pr2[1], pr2[2], pr2[3], pr2[4])
+            listaVendita.append(p4)
+
+
+        for i, p5 in enumerate(listaVendita):
+            p5.setPezzi(listaPrV[i])
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return render_template("recap.html", lista = listaVendita)
+
 if __name__ == '__main__':
 
    app.run(debug = True)
